7f14403983d7a1ca1dda83c7d7f5ddf028b77dbd
zanllp <qc@zanllp.cn>
2022-02-15 17:07:26 +0800

    feat: 支持推导非module的state，dispatch，commit，新增几个函数用来简化用法

--- example/src/store/index.ts
+++ example/src/store/index.ts
@@ -1,24 +1,28 @@
-import { createStore, Module } from 'vuex'
+import { createStore } from 'vuex'
 import cart from './modules/cart'
 import products from './modules/products'
-import {  Modules2RootState, DispatchOverloadDict } from '../../../'
+import { GetRootState, GetDispatchOverload, GetCommitOverload, GetOverloadDict } from '../../../'
+import { Store } from 'vuex'
 
-const modules = {
+const storeParams = {
+  modules: {
     cart,
     products
+  },
+  state: {
+    hello : 'world'
+  },
+  mutations: {
+    setHello (s: any, foo: string) {
+
+    }
+  }
 }
+type StoreParams = typeof storeParams
+const store = createStore(storeParams) as Store<GetRootState<StoreParams>>
 
-type Modules = typeof modules
-type RS = Modules2RootState<Modules>
-const store = createStore<RS>({
-    modules
-})
-
-
-type ActionDict = DispatchOverloadDict<Modules, 'actions'>
-type ActionTypes = keyof ActionDict
-
-export const dispatch : <T extends ActionTypes> (type: T, ...args: Parameters<ActionDict[T]>) => ReturnType<ActionDict[T]> = store.dispatch as any
+export const dispatch = store.dispatch as GetDispatchOverload<StoreParams> // 单独导出相对hook能检查的更严格
+export const commit = store.commit as GetCommitOverload<StoreParams>
 
 dispatch('cart/addProductToCart', {
     id: 2,
@@ -31,5 +35,23 @@ dispatch('products/getAllProducts').then(all => {
     console.log(all.map(prod => prod.title).join())
 })
 dispatch('products/getone', 1)
+
+commit('setHello', 'bar')
+
+
+/**
+ * 可选的给hook函数增加重载
+ */
+declare module 'vuex' {
+  type MutationDict = GetOverloadDict<StoreParams, 'mutations'>
+  type ActionDict = GetOverloadDict<StoreParams, 'actions'>
+  export interface Dispatch {
+    <T extends keyof ActionDict> (type: T, ...args: Parameters<ActionDict[T]>): ReturnType<ActionDict[T]>
+  }
+  export function useStore<S = GetRootState<StoreParams>>(): Store<S>
+  export interface Commit {
+    <T extends keyof MutationDict> (type: T, ...args: Parameters<MutationDict[T]>) : void
+  }
+}
 
 export default store
